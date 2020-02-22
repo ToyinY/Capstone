a100c13b267f3b96f911a9a49c07115bd5986b05
MfonisoInoyo <34751785+MfonisoInoyo@users.noreply.github.com>
2020-02-22 13:43:05 -0500

    Update timer.py
    Display elapsed time of workout on Blynk

--- timer.py
+++ timer.py
@@ -10,7 +10,7 @@ import blynklib
 import board
 
 # Set up Blynk
-BLYNK_AUTH= "n3rlQoz_veIonWxZG-rfLqQoUh75BnQW"
+BLYNK_AUTH= "p78reEHRbPpjn24FxRPz9Jpu0dFdVtvJ"
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
 
@@ -25,33 +25,48 @@ timer = blynktimer.Timer()
 
 
 # Write accelerometer values to app 
-@timer.register(vpin_num=22, interval=1, run_once= False)
-@timer.register(vpin_num=23, interval=1, run_once= False)
-@timer.register(vpin_num=24, interval=1, run_once= False)
-@timer.register(vpin_num=25, interval=1, run_once= False)
-def write_acc_to_virtual_pin(vpin_num=1):
-    x, y, z, orientation = read_acc()
-    if vpin_num == 22:
-        print(WRITE_EVENT_PRINT_MSG.format(vpin_num, x))
-        blynk.virtual_write(vpin_num, x)
-    if vpin_num == 23:
-        print(WRITE_EVENT_PRINT_MSG.format(vpin_num, y))
-        blynk.virtual_write(vpin_num, y)
-    if vpin_num == 24:
-        print(WRITE_EVENT_PRINT_MSG.format(vpin_num, z))
-        blynk.virtual_write(vpin_num, z)
-    if vpin_num == 25:
-        print(WRITE_EVENT_PRINT_MSG.format(vpin_num, orientation))
-        blynk.virtual_write(vpin_num, orientation)
+##@timer.register(vpin_num=22, interval=1, run_once= False)
+##@timer.register(vpin_num=23, interval=1, run_once= False)
+##@timer.register(vpin_num=24, interval=1, run_once= False)
+##@timer.register(vpin_num=25, interval=1, run_once= False)
+##def write_acc_to_virtual_pin(vpin_num=1):
+##    x, y, z, orientation = read_acc()
+##    if vpin_num == 22:
+##        print(WRITE_EVENT_PRINT_MSG.format(vpin_num, x))
+##        blynk.virtual_write(vpin_num, x)
+##    if vpin_num == 23:
+##        print(WRITE_EVENT_PRINT_MSG.format(vpin_num, y))
+##        blynk.virtual_write(vpin_num, y)
+##    if vpin_num == 24:
+##        print(WRITE_EVENT_PRINT_MSG.format(vpin_num, z))
+##        blynk.virtual_write(vpin_num, z)
+##    if vpin_num == 25:
+##        print(WRITE_EVENT_PRINT_MSG.format(vpin_num, orientation))
+##        blynk.virtual_write(vpin_num, orientation)
 
 # Read accelerometer values 
-def read_acc():
-    x,y,z = sensor.acceleration
-    #print(READ_PRINT_MSG.format(pin))
-    orientation = sensor.orientation 
-    #time.sleep(1)
-    return x, y, z, orientation
+##def read_acc():
+##    x,y,z = sensor.acceleration
+##    #print(READ_PRINT_MSG.format(pin))
+##    orientation = sensor.orientation 
+##    #time.sleep(1)
+##    return x, y, z, orientation
+
+# Write time elapsed value to app 
+@timer.register(vpin_num=0, interval=1, run_once= False)
+def start_timer(vpin_num=0):
+    start = time.perf_counter()
+    return start
+
+start_time = start_timer(0)
 
 while True:
     blynk.run()
-    timer.run() 
+    timer.run()
+
+    seconds = round(time.perf_counter() - start_time)
+    m,s = divmod(seconds, 60)
+    h,m = divmod(m, 60)
+    elapsed_time = '{:d}:{:02d}:{:02d}'.format(h,m,s)
+    #print(WRITE_EVENT_PRINT_MSG.format(0, elapsed_time))
+    blynk.virtual_write(0, elapsed_time)
